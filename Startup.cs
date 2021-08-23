043780f06fd58ec20d60d58cf0c802309815ed8b
Claire Botman <marshy@zeta.com.au>
2021-08-24 07:41:48 +0800

    rename graphql project & move db connection string to config

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using HotChocolate.Types;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Server.DataLoader;
@@ -16,11 +17,18 @@ namespace Server
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddPooledDbContextFactory<ApplicationDbContext>(options => options.UseSqlite("Data Source=lets.db").LogTo(Console.WriteLine));
+            services.AddPooledDbContextFactory<ApplicationDbContext>(options => options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")).LogTo(Console.WriteLine));
             services
                 .AddGraphQLServer()
                 .AddType(new UuidType('D'))
